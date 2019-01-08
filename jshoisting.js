a140419a86ada456c1cc8f21c913b5a18bc12fc3
Greg <greg@Gregs-MacBook-Air.local>
2019-01-07 16:19:09 -0800

    done

---
+++ jshoisting.js
@@ -0,0 +1,66 @@
+// var hello;
+// console.log(hello) //logs undefined
+// hello = 'world'
+
+// var needle;
+// needle = 'haystack'
+// test();  //output is magnet
+// function test(){
+//   var needle;
+//   needle = 'magnet'
+//   console.log(needle); //would output magnet
+// }
+
+// var brendan;
+// brendan = 'super cool'
+// function print(){  //never actually called
+//   brenden = 'only okay'
+//   console.log(brendan)
+// }
+// console.log(brendan) //will output super cool
+
+// var food;
+// food = 'chicken'
+// eat(); //will output 'half-chicken'
+// function eat(){
+//   var food;
+//   food = 'half-chicken'
+//   console.log(food)
+//   food = 'gone'
+// }
+
+// mean(); //will  not call
+// console.log(food) // undefined
+// var mean = function(){
+//   var food;
+//   food = 'chicken';
+//   console.log(food) //would log chicken
+//   food = "fish"
+//   console.log(food) //would log fish
+  
+// }
+// console.log(food) //undefined and will break before getting here
+
+// var genre;
+// console.log(genre) //log undefined
+// rewind() // logs 'rock' and 'r&b'
+// function rewind(){
+//   var genre;
+//   genre = 'rock';
+//   console.log(genre) // will log 'rock'
+//   genre = "r&b";
+//   console.log(genre) // will log "r&b"
+// }
+// console.log(genre) // undefined
+
+// dojo = 'san jose'
+// console.log(dojo) // undefined I think - nope, it knows its san jose
+// learn() // seattle and burbank
+// function learn() {
+//   var dojo;
+//   dojo = "seattle"
+//   console.log(dojo)
+//   dojo = "burbank"
+//   console.log(dojo)
+// }
+// console.log(dojo) // undefined I think - - nope, it knows its san jose
